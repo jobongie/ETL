2dc41d200ced42ba60988e2f84e57f9be165d43e
jobongie <68445190+jobongie@users.noreply.github.com>
2020-10-27 22:09:02 -0500

    Images of postgresql databases

--- PostgreSQL/query.sql
+++ PostgreSQL/query.sql
@@ -1,5 +1,5 @@
 
--- Create the first table
+-- -- Create the first table
 -- CREATE TABLE "states_winner_df"(
 --     "State" VARCHAR,
 -- 	"Pollster" VARCHAR,
@@ -9,11 +9,11 @@
 --     "Trump Adjusted" DEC,
 -- 	"Winner" VARCHAR);
 
--- Check data import
+-- -- Check data import
 -- SELECT *
 -- FROM states_winner_df;
 
--- Create the second table
+-- -- Create the second table
 -- CREATE TABLE "short_results_df" (
 --     "State" VARCHAR(20),
 --     "Electoral Votes" INT);
@@ -22,7 +22,7 @@
 -- SELECT *
 -- FROM short_results_df;
 
--- See both tables together
+-- -- See both tables together
 -- SELECT DISTINCT X."State", X."Pollster", X."Clinton", X."Trump", 
 -- 				  X."Clinton Adjusted", X."Trump Adjusted", X."Winner", Y."Electoral Votes"
 -- INTO states_electoral_df
@@ -30,7 +30,7 @@
 -- LEFT OUTER JOIN short_results_df as Y 
 -- ON Y."State" = X."State"
 
---View final table
+-- --View final table
 -- SELECT *
 -- FROM states_electoral_df
 
