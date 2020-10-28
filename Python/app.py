b25a10f7b010bc502af54caed1c6900f7bf02beb
npipkins <68622679+npipkins@users.noreply.github.com>
2020-10-27 21:27:01 -0500

    updates for flask

--- Python/app.py
+++ Python/app.py
@@ -1,33 +1,41 @@
-# 1. import Flask and dependencies
+# import Flask and dependencies
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-from models import db
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
-POSTGRES = {
-    'user': 'postgres',
-    'pw': 'password',
-    'db': 'my_database',
-    'host': 'localhost',
-    'port': '5432',
-}
+dbuser = 'postgres'
+dbpassword = 'pip'
+dbhost = 'localhost'
+dbport = '5432'
+dbname= 'ETL Project Work'
 
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-db.init_app(app)
+engine = create_engine(f"postgres://{dbuser}:{dbpassword}@{dbhost}:{dbport}/{dbname}")
 
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+states = Base.classes.states_electoral_df
 
 #################################################
 # Flask Routes
 #################################################
-@app.route("/api/v1.0/states_electoral_df")
+@app.route("/")
 def states_electoral_df():
-    results = session.query(states_electoral_df).all()
-    results_list = list(results)
-    return jsonify(results_list)
+    session = Session(engine)
+    
+    results_df = pd.read_sql(session.query(states.State, states.Pollster, states.Clinton, states.Trump, states.clinton_adjusted, states.trump_adjusted, states.Winner, states.electoral_votes).statement, con=engine)
+    
+
+
+    session.close()
+    return jsonify(results_df.to_dict(orient='records'))
 
 
 
